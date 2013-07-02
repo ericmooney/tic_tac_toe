508fd5a9c3d9ce0ee508f9a39aa6c6e5175bba1f
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-01 19:44:03 -0700

    Basic functionality added without game win function

--- js/script.js
+++ js/script.js
@@ -11,8 +11,7 @@ $(document).ready(function(){
 
 
 
-  // need to determine which player's turn it is, perhaps an
-  // "even" or "odd" counter?
+  // need to determine which player's turn it is with even/odd counter:
   var choose_player = function() {
     if (turn_counter % 2 == 0) {
       active_player.push(player_x);
@@ -31,8 +30,8 @@ $(document).ready(function(){
   };
 
 
-  // when player x chooses tile, it should append an "x" to
-  // the (div #board .tile) tile and add a class "x" to the tile
+  // when player x chooses tile, it appends an "x" to
+  // the (div #board .tile) and adds a class "active" and "x" to the tile
   // and passes the id of that div into a player x array container
 
   var handle_click = function() {
@@ -42,10 +41,11 @@ $(document).ready(function(){
     }
     activate_tile(tile);
 
-
+    // this is what I'm working on right now
     // if (is_game_over()) {
     //   handle_win();
     // }
+
 
     increment_turn_counter();
     choose_player();
@@ -57,11 +57,13 @@ $(document).ready(function(){
 
     if (active_player == "x") {
       player_x_array.push(tile.attr('id'));
+      player_x_array.sort();
       tile.addClass('x');
       tile.append('x');
     }
     else if (active_player == "o") {
       player_o_array.push(tile.attr('id'));
+      player_o_array.sort();
       tile.addClass('o');
       tile.append('o');
     }
@@ -69,17 +71,67 @@ $(document).ready(function(){
     console.log(player_o_array);
   };
 
-  //then, it needs to see if the players array now is equal to one of the 8 possible "win" arrays
-  // if it does not, add to the "increment_turn counter" and wait for next click
+  // then, it needs to see if the players array now is equal to one of the 8 possible "win" arrays
+
+
+  // ****I need to do more work around handling this possibility - especially in the var handle_win
+  // and need to add to is_game_over possibilities -> || no_more_open_tiles()
+  // var no_more_open_tiles = function() {
+  //   return ($('.tile.active').size() == tiles.length);
+  // }
+
+
+// loop through winning array, then,push any matching values into an array
+  // var is_player_x_winner = function(player_x_array) {
+
+  //   var all_winning_arrays = [
+  //     ["0","1","2"]
+  //     // ["3","4","5"],
+  //     // ["6","7","8"],
+  //     // ["0","3","6"],
+  //     // ["1","4","7"],
+  //     // ["2","5","8"],
+  //     // ["0","4","8"],
+  //     // ["2","4","6"]
+  //   ];
+
+  //   for (var x = 0; x < all_winning_arrays.length; x++) {
+  //     var winning_array =
+  //     for (var i = 0; i < player_x_array.length; i++) {
+  //       var potential_win = [];
+  //       if ($.inArray(i, x) > -1) {
+  //         potential_win.push(i);
+  //       }
+  //         if (potential_win.length == 3){
+  //           return true;
+  //         }
+  //     }
+  //   }
+  // };
 
   // var is_game_over = function() {
-  //   return ($('.tile.matched').size() == tiles.length);
+  //   return three_in_a_row();
   // };
 
-  // var handle_win = function() {
-  //   alert(active_player + 'You Win!');
-  //   window.location.href = window.location.href;
+  // var three_in_a_row = function(){
+  //   if (is_player_x_winner() === true){
+  //     return true;
+  //   }
+  //   else if (is_player_o_winner() === true) {
+  //     return true;
+  //   }
+  //   else {
+  //     return false;
+  //   }
   // };
+
+
+
+
+  var handle_win = function() {
+    alert('Hey ' + active_player + ', You Win!');
+    window.location.href = window.location.href;
+  };
 
   $.each(tiles, function(index, tile) {
     var tile = $(tile);
