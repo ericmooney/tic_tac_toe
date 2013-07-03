6f934928aef72ba34fdeb0325726e56d18737d12
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-02 18:06:35 -0700

    Add win handler and tie handler

--- js/script.js
+++ js/script.js
@@ -9,18 +9,27 @@ $(document).ready(function(){
 
   var tiles = $('.tile');
 
-
+  var all_winning_arrays = [
+    ["0","1","2"],
+    ["3","4","5"],
+    ["6","7","8"],
+    ["0","3","6"],
+    ["1","4","7"],
+    ["2","5","8"],
+    ["0","4","8"],
+    ["2","4","6"]
+  ];
 
   // need to determine which player's turn it is with even/odd counter:
   var choose_player = function() {
     if (turn_counter % 2 == 0) {
       active_player.push(player_x);
-      var player = $('#player_x')
+      var player = $('#player_x');
       player.addClass('your_turn');
       player.append("<p> - It's your move!</p>");
     } else {
       active_player.push(player_o);
-      var player = $('#player_o')
+      var player = $('#player_o');
       player.addClass('your_turn');
       player.append("<p> - It's your move!</p>");
     }
@@ -43,6 +52,10 @@ $(document).ready(function(){
   // the (div #board .tile) and adds a class "active" and "x" to the tile
   // and passes the id of that div into a player x array container
 
+  var no_more_open_tiles = function() {
+    return ($('.tile.active').size() == tiles.length);
+  };
+
   var handle_click = function() {
     var tile = $(this);
     if (tile.hasClass('active')){
@@ -50,11 +63,12 @@ $(document).ready(function(){
     }
     activate_tile(tile);
 
-    // this is what I'm working on right now
-    // if (is_game_over()) {
-    //   handle_win();
-    // }
-
+    if (three_in_a_row() === true) {
+      handle_win();
+    }
+    else if (no_more_open_tiles()) {
+      handle_draw();
+    }
 
     increment_turn_counter();
     choose_player();
@@ -76,66 +90,44 @@ $(document).ready(function(){
       tile.addClass('o');
       tile.append('o');
     }
-    console.log(player_x_array);
-    console.log(player_o_array);
   };
 
   // then, it needs to see if the players array now is equal to one of the 8 possible "win" arrays
+  // loop through winning array, then,push any matching values into an array
+
+  var is_winner = function(player_array) {
+    for (var i = 0; i < all_winning_arrays.length; i++) {
+      var potential_win = [];
+      var winning_array = all_winning_arrays[i];
+      for(var x = 0; x < winning_array.length; x++) {
+        var needle = winning_array[x];
+        var haystack = player_array;
+        if ($.inArray(needle, haystack) > -1) {
+          potential_win.push(needle);
+          console.log(potential_win);
+          if (potential_win.length == winning_array.length) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  };
 
 
-  // ****I need to do more work around handling this possibility - especially in the var handle_win
-  // and need to add to is_game_over possibilities -> || no_more_open_tiles()
-  // var no_more_open_tiles = function() {
-  //   return ($('.tile.active').size() == tiles.length);
-  // }
+  var three_in_a_row = function(){
+    if (is_winner(player_x_array) === true || is_winner(player_o_array) === true){
+      return true;
+    } else {
+      return false;
+    }
+  };
 
 
-// loop through winning array, then,push any matching values into an array
-  // var is_player_x_winner = function(player_x_array) {
-
-  //   var all_winning_arrays = [
-  //     ["0","1","2"]
-  //     // ["3","4","5"],
-  //     // ["6","7","8"],
-  //     // ["0","3","6"],
-  //     // ["1","4","7"],
-  //     // ["2","5","8"],
-  //     // ["0","4","8"],
-  //     // ["2","4","6"]
-  //   ];
-
-  //   for (var x = 0; x < all_winning_arrays.length; x++) {
-  //     var winning_array =
-  //     for (var i = 0; i < player_x_array.length; i++) {
-  //       var potential_win = [];
-  //       if ($.inArray(i, x) > -1) {
-  //         potential_win.push(i);
-  //       }
-  //         if (potential_win.length == 3){
-  //           return true;
-  //         }
-  //     }
-  //   }
-  // };
-
-  // var is_game_over = function() {
-  //   return three_in_a_row();
-  // };
-
-  // var three_in_a_row = function(){
-  //   if (is_player_x_winner() === true){
-  //     return true;
-  //   }
-  //   else if (is_player_o_winner() === true) {
-  //     return true;
-  //   }
-  //   else {
-  //     return false;
-  //   }
-  // };
-
-
-
+  var handle_draw = function(){
+    alert("It's a draw! :(");
+    window.location.href = window.location.href;
+  };
 
   var handle_win = function() {
     alert('Hey ' + active_player + ', You Win!');
@@ -146,6 +138,8 @@ $(document).ready(function(){
     var tile = $(tile);
     tile.on('click', handle_click);
   });
+
+
 
 
 });
